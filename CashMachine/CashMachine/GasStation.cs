8dae8b0174120d1bf8d87ad0191e32e06d9fd83f
arievasan <alievalmn@gmail.com>
2023-02-25 18:44:34 +0400

    ninja way

---
+++ CashMachine/CashMachine/GasStation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    public class GasStation
+    {
+        public List<Gasoline>? GasolineOption { get; set; } = new();
+        public List<MenuItem>? Menu { get; set; } = new();
+        public GasStation()
+        {
+            GasolineOption.Add(new Gasoline { Type = "А-98", Price = 1.90 });
+            GasolineOption.Add(new Gasoline { Type = "А-95", Price = 1.60 });
+            GasolineOption.Add(new Gasoline { Type = "А-92", Price = 1.30 });
+            GasolineOption.Add(new Gasoline { Type = "А-80", Price = 1.10 });
+
+            Menu.Add(new MenuItem { Name = "Хот-дог", Price = 1.80, Count = 0 });
+            Menu.Add(new MenuItem { Name = "Гамбургер", Price = 2.20, Count = 0 });
+            Menu.Add(new MenuItem { Name = "Картошка-фри", Price = 1.10, Count = 0 });
+            Menu.Add(new MenuItem { Name = "Спрайт", Price = 0.70, Count = 0 });
+
+        }
+    }
+}
