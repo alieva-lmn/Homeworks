92e3c3c59d0736ee1487cfe3ce27dba0d748eba0
arievasan <alievalmn@gmail.com>
2023-04-10 15:14:42 +0400

    Solution added

---
+++ GitHW/GitHW/Realization/CharacterRealization.cs
@@ -0,0 +1,19 @@
+﻿using GitHW.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHW.Realization
+{
+    public class CharacterRealization
+    {
+        Character character = new();
+
+        Character createCharacter()
+        {
+            return character;
+        }
+    }
+}
