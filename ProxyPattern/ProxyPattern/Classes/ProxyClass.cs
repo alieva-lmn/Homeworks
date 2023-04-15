5a243e088617cd46710d473764dd943f34610ba1
arievasan <alievalmn@gmail.com>
2023-04-16 00:59:18 +0400

    Proxy pattern is added

---
+++ ProxyPattern/ProxyPattern/Classes/ProxyClass.cs
@@ -0,0 +1,25 @@
+﻿using ProxyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern.Classes
+{
+    public class ProxyClass : IRecipe
+    {
+        private readonly Pancake _pancake;
+
+        public ProxyClass()
+        {
+            _pancake = new Pancake();
+        }
+
+        public void Recipe()
+        {
+            _pancake.Recipe();
+            Console.WriteLine("Some additional information about pancake recipe from Proxy class");
+        }
+    }
+}
---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,5 @@
+﻿using ProxyPattern.Classes;
+using ProxyPattern.Interfaces;
+
+IRecipe myrecipe = new ProxyClass();
+myrecipe.Recipe();
