2dfe4710b5aa029796f0032a2d04bea45ebf2c11
arievasan <alievalmn@gmail.com>
2023-07-27 01:42:58 +0400

    API HW

---
+++ ApiHW/ApiHW/CatFact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiHW
+{
+    public class CatFact
+    {
+        public string Fact { get; set; }
+    }
+}
---
+++ ApiHW/ApiHW/Program.cs
@@ -0,0 +1,30 @@
+﻿using ApiHW;
+using System.Net.Http.Json;
+
+
+try
+{
+    using (var httpClient = new HttpClient())
+    {
+        string apiUrl = "https://catfact.ninja/fact";
+
+        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+        response.EnsureSuccessStatusCode();
+
+        var catFact = await response.Content.ReadFromJsonAsync<CatFact>();
+
+        if (catFact != null)
+        {
+            Console.WriteLine($"Random fact about kitties: {catFact.Fact}");
+        }
+        else
+        {
+            Console.WriteLine("Something went wrong...");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR ALERT: {ex.Message}");
+}
