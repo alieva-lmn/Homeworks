8c1d9f928667423ff2eb095bfb14e42c143504d8
arievasan <alievalmn@gmail.com>
2023-08-07 00:45:07 +0400

    async crud is added

---
+++ EF_CRUD/EF_CRUD/DBContext/MyStoreContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using EF_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_CRUD.DBContext;
+
+public partial class MyStoreContext : DbContext
+{
+    public MyStoreContext()
+    {
+    }
+
+    public MyStoreContext(DbContextOptions<MyStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-HVUR0JDE; Initial Catalog=MyStore; Integrated Security=True; Connect Timeout=30; TrustServerCertificate=True; Encrypt=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_CRUD/EF_CRUD/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using EF_CRUD.DBContext;
+using EF_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace EF_CRUD
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; set; } = new();
+        private MyStoreContext _context = new();
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            LoadProductsAsync();
+            listViewProducts.ItemsSource = Products;
+        }
+
+        private async void LoadProductsAsync()
+        {
+            Products.Clear();
+
+            var products = await _context.Products.ToListAsync();
+
+            foreach (var product in products)
+            {
+                Products.Add(product);
+            }
+        }
+
+        private async void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var new_product = new Product();
+
+            new_product.Name = txtName.Text;
+            decimal.TryParse(priceName.Text, out decimal decimal_price);
+            new_product.Price = decimal_price;
+            Int32.TryParse(stockName.Text, out int int_stock);
+            new_product.Stock = int_stock;
+
+            _context.Products.Add(new_product);
+            await _context.SaveChangesAsync();
+
+            LoadProductsAsync();
+        }
+
+        private async void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (listViewProducts.SelectedItem is Product selectedProduct)
+            {
+                string new_name = txtName.Text;
+                decimal.TryParse(priceName.Text, out decimal decimal_price);
+                Int32.TryParse(stockName.Text, out int int_stock);
+
+                selectedProduct.Name = new_name;
+                selectedProduct.Price = decimal_price;
+                selectedProduct.Stock = int_stock;
+
+                _context.Products.Update(selectedProduct);
+                await _context.SaveChangesAsync();
+
+                LoadProductsAsync();
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listViewProducts.SelectedItem is Product selectedProduct)
+            {
+                _context.Products.Remove(selectedProduct);
+                await _context.SaveChangesAsync();
+                LoadProductsAsync();
+            }
+        }
+    }
+}
